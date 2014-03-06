79024e024764980487d01901ea3c5f00be7b9f93
Pengfei Zhang <zpfalpc23@gmail.com>
2014-03-06 14:16:21 +0800

    Add the WSGI router for Voltracker v1 API
    this router is used for http server to router the request to specifical
    applications by url address. At current time, we only add the maps of
    volume operations.

---
+++ voltracker/api/v1/router.py
@@ -0,0 +1,57 @@
+# Copyright 2014 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from voltracker.api.v1 import volumes
+from voltracker.common import wsgi
+
+
+class API(wsgi.Router):
+
+    """WSGI router for Voltracker v1 API requests."""
+
+    def __init__(self, mapper):
+        volumes_resource = volumes.create_resource()
+
+        mapper.connect("/volumes",
+                       controller=volumes_resource,
+                       action='index',
+                       conditions={'method': ['GET']})
+        mapper.connect("/volumes/{volume_id}",
+                       controller=volumes_resource,
+                       action='index',
+                       conditions={'method': ['HEAD']})
+        mapper.connect("/volumes/query",
+                       controller=volumes_resource,
+                       action='query',
+                       conditions={'method': ['GET']})
+        mapper.connect("/volumes/{volume_id}",
+                       controller=volumes_resource,
+                       action='register',
+                       conditions={'method': ['POST']})
+        mapper.connect("/volumes/{volume_id}",
+                       controller=volumes_resource,
+                       action='update',
+                       conditions={'method': ['PUT']})
+        mapper.connect("/volumes/{volume_id}",
+                       controller=volumes_resource,
+                       action='delete',
+                       conditions={'method': ['DELETE']})
+        mapper.connect("/volumes",
+                       controller=volumes_resource,
+                       action="delete",
+                       conditions={'method': ['DELETE']})
+
+        super(API, self).__init__(mapper)
